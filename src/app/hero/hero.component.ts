fdc3b5df4745ea7f6174ce23fe1612b54caaff08
Tristan Zander <knavetnz@gmail.com>
2021-09-19 21:17:57 -0400

    Image lerp now uses animation api

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -12,66 +12,84 @@ export class HeroComponent implements OnInit, OnDestroy {
 
   @ViewChild("container") container: ElementRef<HTMLDivElement>;
 
-  lerpFactor = 50;
-  positionToMoveTo: { x: number, y: number } = { x: 0, y: 0 };
+  lerpFactor = 5;
+  positionToMoveTo: { x: number, y: number } | undefined;
+
+  updateInterval: any;
 
   constructor(container: ElementRef<HTMLDivElement>, image: ElementRef<HTMLImageElement>) {
     this.container = container;
     this.image = image;
   }
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    // clearInterval(this.updateInterval)
   }
 
   ngOnInit(): void {
+    // this.updateInterval = setInterval(() => {
 
-    this.positionToMoveTo = {
-      x: this.image.nativeElement.x,
-      y: this.image.nativeElement.y
+    //   if (this.positionToMoveTo === undefined) {
+    //     return;
+    //   }
+
+    //   // const lerpX = (this.image.nativeElement.offsetLeft - this.image.nativeElement.width / 2) * (1.0 / this.lerpFactor);
+    //   // const lerpY = (this.image.nativeElement.offsetTop - this.image.nativeElement.height / 2) * (1.0 / this.lerpFactor);
+
+    //   // TODO: Use requestAnimationFrame instead
+
+    //   const imageCenterX = this.image.nativeElement.offsetLeft + (this.image.nativeElement.width / 2)
+    //   const imageCenterY = this.image.nativeElement.offsetTop + (this.image.nativeElement.height / 2)
+
+
+    //   const deltaX = this.positionToMoveTo.x - imageCenterX;
+    //   const deltaY = this.positionToMoveTo.y - imageCenterY;
+
+
+    //   this.image.nativeElement.style.setProperty("--x", this.image.nativeElement.x + (1.0 / this.lerpFactor) * (deltaX) + "px");
+    //   this.image.nativeElement.style.setProperty("--y", this.image.nativeElement.y + (1.0 / this.lerpFactor) * (deltaY) + "px");
+
+    // }, 1000 / 60)
+
+    let last: DOMHighResTimeStamp, deltaTime = 0;
+
+    const lerp = (timestamp: DOMHighResTimeStamp) => {
+      console.debug("running")
+      deltaTime = (timestamp - last) / 1000;
+      last = timestamp;
+      if (this.positionToMoveTo === undefined) {
+        window.requestAnimationFrame(lerp)
+      } else {
+
+        const imageCenterX = this.image.nativeElement.offsetLeft + (this.image.nativeElement.width / 2)
+        const imageCenterY = this.image.nativeElement.offsetTop + (this.image.nativeElement.height / 2)
+
+
+        const deltaX = this.positionToMoveTo.x - imageCenterX;
+        const deltaY = this.positionToMoveTo.y - imageCenterY;
+
+
+        this.image.nativeElement.style.setProperty("--x", this.image.nativeElement.x + (this.lerpFactor) * (deltaX) * deltaTime  + "px");
+        this.image.nativeElement.style.setProperty("--y", this.image.nativeElement.y + (this.lerpFactor) * (deltaY) * deltaTime  + "px");
+
+        window.requestAnimationFrame(lerp)
+      }
     }
 
-    let interval = setInterval(() => {
-      // console.debug("interval!")
-      // const lerpX = (this.image.nativeElement.offsetLeft - this.image.nativeElement.width / 2) * (1.0 / this.lerpFactor);
-      // const lerpY = (this.image.nativeElement.offsetTop - this.image.nativeElement.height / 2) * (1.0 / this.lerpFactor);
-
-      // TODO: Use requestAnimationFrame instead
-
-      const imageCenterX = this.image.nativeElement.offsetLeft + (this.image.nativeElement.width / 2)
-      const imageCenterY = this.image.nativeElement.offsetTop + (this.image.nativeElement.height / 2)
-
-
-      const deltaX = this.positionToMoveTo.x - imageCenterX;
-      const deltaY = this.positionToMoveTo.y - imageCenterY;
-      
-
-      this.image.nativeElement.style.setProperty("--x", this.image.nativeElement.x + ( 1.0 / this.lerpFactor) * (deltaX) + "px");
-      this.image.nativeElement.style.setProperty("--y", this.image.nativeElement.y + ( 1.0 / this.lerpFactor) * (deltaY) + "px");
-
-    }, 1 / 144)
+    window.requestAnimationFrame(lerp.bind(this))
   }
 
   parallax(event: MouseEvent) {
-    // console.debug(event)
-    // console.log(this.image)
-
-    // console.log(event.clientX, event.clientY)
-    // console.log(event.x, event.y)
-
-    // const mouseXPos = event.x - this.container.nativeElement.offsetLeft - (this.image.nativeElement.width / 2)
-    // const mouseYPos = event.y - this.container.nativeElement.offsetTop - (this.image.nativeElement.height / 2)
-
-    // const lerpX = mouseXPos - (this.image.nativeElement.offsetLeft - this.image.nativeElement.width / 2) * this.lerpFactor;
-    // const lerpY = mouseYPos - (this.image.nativeElement.offsetTop - this.image.nativeElement.height / 2) * this.lerpFactor;
-
-    // this.image.nativeElement.style.setProperty("--x", this.image.nativeElement.offsetLeft + lerpX + "px");
-    // this.image.nativeElement.style.setProperty("--y", mouseYPos + "px");
-
     this.positionToMoveTo = {
       x: event.x,
       y: event.y
     }
+  }
 
+  mouseExit(event: MouseEvent) {
+    this.positionToMoveTo = {
+      x: this.container.nativeElement.clientWidth / 2
+      , y: this.container.nativeElement.clientHeight / 2
+    }
   }
 
 }
